a13e4cfcf0ca12f9fc213c2fb228814e66b7217e
Chris Spencer <chrisspen@gmail.com>
2022-09-13 23:45:51 -0400

    Cleaned up publish script.

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+echo "[$(date)] Initializing environment."
 [ -d .env ] && rm -Rf .env
 python3.9 -m venv .env
 . .env/bin/activate
 pip install -U pip
+pip install -U setuptools wheel
 pip install -r pip-requirements.txt -r pip-requirements-test.txt
 pre-commit install
